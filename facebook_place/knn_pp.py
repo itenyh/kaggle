cb43aef2ffa14a01199365a74bac1a85911e3b86
YangHan <yanghan@YangHandeMacBook-Air.local>
2016-06-23 20:31:11 +0800

    sadf

--- facebook_place/knn_pp.py
+++ facebook_place/knn_pp.py
@@ -75,7 +75,7 @@ def process_one_cell(df_cell_train, df_cell_test, fw, th):
     # param = {'max_depth':2, 'eta':1, 'silent':1, 'objective':'binary:logistic' }
     # bst = xgb.train(param, ,label = y)
 
-    clf_list = [clf_bagging_knn, clf_rf]
+    clf_list = [clf_bagging_knn]
     weight = [lr(0.64), lr(0.65)]
 
     y_pred_all = []
@@ -170,7 +170,7 @@ def process_grid(df_train, df_test, size, x_step, y_step, x_border_augment, y_bo
 
     #Writting to csv
     ds_sub.name = 'place_id'
-    ds_sub.to_csv('sub_bagknn-rf-season_pp.csv', index=True, header=True, index_label='row_id')
+    ds_sub.to_csv('sub_bagknn-pp.csv', index=True, header=True, index_label='row_id')
 
 
 ##########################################################
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     df_train['hour'] = df_train['time']//60
     df_train['weekday'] = df_train['hour']//24
     df_train['month'] = df_train['weekday']//30
-    df_train['season'] = (df_train['month'] + 2)//3 % 4
+    # df_train['season'] = (df_train['month'] + 2)//3 % 4
     df_train['year'] = (df_train['weekday']//365+1)*fw[5]
 
     df_train['hour'] = ((df_train['hour']%24+1)+minute/60.0)
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     df_test['hour'] = df_test['time']//60
     df_test['weekday'] = df_test['hour']//24
     df_test['month'] = df_test['weekday']//30
-    df_test['season'] = (df_test['month'] + 2)//3 % 4
+    # df_test['season'] = (df_test['month'] + 2)//3 % 4
     df_test['year'] = (df_test['weekday']//365+1)*fw[5]
     df_test['hour'] = ((df_test['hour']%24+1)+minute/60.0)*fw[2]
     df_test['weekday'] = (df_test['weekday']%7+1)*fw[3]
